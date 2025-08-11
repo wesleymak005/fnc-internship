dd70d564d677ab20b43335f73e12069ce04cc8d8
Wesley Mak <wesleymak005@gmail.com>
2025-08-10 20:15:25 -0400

    Tweaked links and layout

--- client/frontend/src/app/topics/hub/page.js
+++ client/frontend/src/app/topics/hub/page.js
@@ -18,57 +18,90 @@ export default function Home() {
             steps needed to break into the field, and how to stay healthy when working in this field.
           </div>
           <div className="flex gap-[20px] max-w-[90%] justify-evenly">
-            <Link className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] hover:bg-[#808080] p-[50px] hover:rounded-[15px] min-w-[33%] justify-center items-center" href="/topics/hub/future">
-              The Future of AI in Software Engineering
-              <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
-                <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
-                <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
-                <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
-                <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
-                <li>a</li>
-                <li>b</li>
-                <li>c</li>
-                <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
-                <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
-                <div>Etiam maximus purus at lorem tristique aliquam. </div>
-                <div>In id fringilla neque.</div>
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center" href="/topics/hub/future">
+                <Image 
+                  className="dark:invert mb-[30px]"
+                  src="/robot-outline.svg"
+                  alt="ai"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                The Future of AI in Software Engineering
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+                  <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
+                  <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
+                  <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
+                  <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
+                  <li>a</li>
+                  <li>b</li>
+                  <li>c</li>
+                  <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
+                  <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
+                  <div>Etiam maximus purus at lorem tristique aliquam. </div>
+                  <div>In id fringilla neque.</div>
+                </div>
               </div>
-            </Link>
-            <Link className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] hover:bg-[#808080] p-[50px] hover:rounded-[15px] min-w-[33%] justify-center items-center" href="/topics/hub/career">
-              Learning Skills and Employment in Software Engineering
-              <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
-                <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
-                <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
-                <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
-                <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
-                <li>a</li>
-                <li>b</li>
-                <li>c</li>
-                <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
-                <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
-                <div>Etiam maximus purus at lorem tristique aliquam. </div>
-                <div>In id fringilla neque.</div>
+              <Link className="flex justify-center bg-[#F0E4D3] hover:bg-[#808080] font-bold text-[30px] p-[50px] rounded-[15px] hover:rounded-[15px]" href="/topics/hub/future">Learn More</Link>
+            </div>
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center" href="/topics/hub/future">
+                <Image 
+                  className="dark:invert mb-[30px]"
+                  src="/tools.svg"
+                  alt="ai"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                Learning Skills and Employment
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+                  <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
+                  <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
+                  <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
+                  <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
+                  <li>a</li>
+                  <li>b</li>
+                  <li>c</li>
+                  <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
+                  <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
+                  <div>Etiam maximus purus at lorem tristique aliquam. </div>
+                  <div>In id fringilla neque.</div>
+                </div>
               </div>
-            </Link>
-            <Link className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] hover:bg-[#808080] p-[50px] hover:rounded-[15px] min-w-[33%] justify-center items-center" href="/topics/hub/health">
-              Staying Healthy as a Software Engineer
-              <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
-                <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
-                <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
-                <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
-                <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
-                <li>a</li>
-                <li>b</li>
-                <li>c</li>
-                <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
-                <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
-                <div>Etiam maximus purus at lorem tristique aliquam. </div>
-                <div>In id fringilla neque.</div>
+              <Link className="flex justify-center bg-[#F0E4D3] hover:bg-[#808080] font-bold text-[30px] p-[50px] rounded-[15px] hover:rounded-[15px]" href="/topics/hub/future">Learn More</Link>
+            </div>
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center" href="/topics/hub/career">
+                <Image 
+                  className="dark:invert mb-[30px]"
+                  src="/plus-box.svg"
+                  alt="ai"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                Staying Healthy as a Software Engineer
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+                  <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
+                  <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
+                  <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
+                  <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
+                  <li>a</li>
+                  <li>b</li>
+                  <li>c</li>
+                  <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
+                  <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
+                  <div>Etiam maximus purus at lorem tristique aliquam. </div>
+                  <div>In id fringilla neque.</div>
+                </div>
               </div>
-            </Link>
+              <Link className="flex justify-center bg-[#F0E4D3] hover:bg-[#808080] font-bold text-[30px] p-[50px] rounded-[15px] hover:rounded-[15px]" href="/topics/hub/health">Learn More</Link>
+            </div>
         </div>
         </div>
       </main>
