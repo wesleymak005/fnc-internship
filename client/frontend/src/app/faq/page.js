d70fe810f3a15a498241b15051d1dd5979a4d30e
leoli7744 <leoli7744@gmail.com>
2025-08-13 03:13:44 -0400

    about us page complete and minor rework for the all topics page

--- client/frontend/src/app/faq/page.js
+++ client/frontend/src/app/faq/page.js
@@ -3,37 +3,75 @@ import Image from "next/image";
 export default function Home() {
   return (
     
-    <div className="font-sans flex flex col items-center justify-center min-h-screen ">
+    <div className="font-sans flex justify-center min-h-screen ">
       
-      <main className="flex gap-[32px] items-center">
-        <div className="flex flex-col gap-[32px] max-w-[50%]">
-          <div className="flex text-[50px]">
-            WIP
+      <main className="flex gap-[32px] mt-[10%] justify-center">
+        <div className="flex flex-col gap-[32px] items-center">
+          <div className="flex text-[50px] justify-center">
+            About Us
           </div>
 
-          <div className="flex">
-            Information to help kick start your journey as a software engineer
+          <div className="flex text-[20px] max-w-[50%] items-center">
+          Hello, and welcome to our website! We are undergraduate software engineering 
+          students in the C2L-PHL program under the Federation of Neighborhood Centers, 
+          creating this site to share our research. Our goal is to support first- and second-year 
+          students in similar situations who may feel uncertain about their path. We aim to bridge 
+          the gap between what’s taught in college and the realities of entering the workforce—covering 
+          skills to learn, what companies seek in interns, and the health implications of a software 
+          engineering career. We also address the current AI boom, clarifying its impact on the field’s 
+          future.
           </div>
-
-          <div className="flex gap-[32px]">
-            <button type="button" className="bg-[#D3D3D3] p-[10px] border hover:bg-[#808080] hover:rounded-[10px] rounded-[10px]">
-              Start Reading
-            </button>
-            <button type="button" className="bg-[#D3D3D3] p-[10px] border hover:bg-[#808080] hover:rounded-[10px] rounded-[10px]">
-              Browse FAQ
-            </button>
-          </div>
+          <div className="flex gap-[20px] max-w-[90%] justify-evenly pt-10">
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center">
+                <Image 
+                  className="dark:invert mb-[30px]"
+                  src="/boy.png"
+                  alt="boy"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                Wesley Mak
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px] h-[100px] text-center">
+                  <div>Class of 2028 at Drexel University </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center">
+                <Image 
+                  className="dark:invert mb-[30px]"
+                  src="/boy.png"
+                  alt="boy"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                Max Tran
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px] h-[100px] text-center">
+                  <div>Class of 2027 at the University of</div>
+                  <div>Pennsylvania</div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center">
+                <Image 
+                  className="dark:invert mb-[30px]"
+                  src="/boy.png"
+                  alt="boy"
+                  width={100}
+                  height={100}
+                  priority
+                />
+                Leo Li
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px] h-[100px] text-center">
+                  <div>Class of 2028 at Drexel University </div>
+                </div>
+              </div>
+            </div>
         </div>
-        <div className="flex flex-col">
-          Placeholder for img
-           <Image
-          className="dark:invert"
-          src="/sign-caution.svg"
-          alt="wip"
-          width={100}
-          height={100}
-          priority
-        />
         </div>
       </main>
     </div>
--- client/frontend/src/app/topics/hub/page.js
+++ client/frontend/src/app/topics/hub/page.js
@@ -19,7 +19,7 @@ export default function Home() {
           </div>
           <div className="flex gap-[20px] max-w-[90%] justify-evenly">
             <div className="flex flex-col gap-[20px]">
-              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] w-[400px] h-[700px] justify-start items-center overflow-y-auto">
                 <Image 
                   className="dark:invert mb-[30px]"
                   src="/robot-outline.svg"
@@ -47,7 +47,7 @@ export default function Home() {
               <Link className="flex justify-center bg-[#F0E4D3] hover:bg-gray-100 font-bold text-[30px] p-[50px] rounded-[15px] hover:rounded-[15px]" href="/topics/hub/future">Learn More</Link>
             </div>
             <div className="flex flex-col gap-[20px]">
-              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] w-[400px] h-[700px] justify-start items-center overflow-y-auto">
                 <Image 
                   className="dark:invert mb-[30px]"
                   src="/tools.svg"
@@ -57,7 +57,7 @@ export default function Home() {
                   priority
                 />
                 Learning Skills and Employment
-                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px] h-[580px]">
                   <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
                   <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
                   <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
@@ -75,7 +75,7 @@ export default function Home() {
               <Link className="flex justify-center bg-[#F0E4D3] hover:bg-gray-100 font-bold text-[30px] p-[50px] rounded-[15px] hover:rounded-[15px]" href="/topics/hub/career">Learn More</Link>
             </div>
             <div className="flex flex-col gap-[20px]">
-              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] min-w-[33%] justify-center items-center">
+              <div className="flex flex-col text-[20px] rounded-[15px] bg-[#FAF7F3] p-[50px] w-[400px] h-[700px] justify-start items-center overflow-y-auto">
                 <Image 
                   className="dark:invert mb-[30px]"
                   src="/plus-box.svg"
@@ -85,19 +85,17 @@ export default function Home() {
                   priority
                 />
                 Staying Healthy as a Software Engineer
-                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px]">
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
-                  <div>Integer quis mauris malesuada, accumsan nunc consectetur, aliquam quam. </div>
-                  <div>Quisque quis dui luctus quam maximus sollicitudin. </div>
-                  <div>Nullam vestibulum pharetra tellus quis vestibulum. </div> 
-                  <div>Nullam lacinia turpis nisl, vitae rhoncus ex suscipit ac. </div>
-                  <li>a</li>
-                  <li>b</li>
-                  <li>c</li>
-                  <div>Pellentesque accumsan metus non tincidunt feugiat. </div>
-                  <div>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </div>
-                  <div>Etiam maximus purus at lorem tristique aliquam. </div>
-                  <div>In id fringilla neque.</div>
+                <div className="flex flex-col justify-start self-start border-t border-gray min-w-[90%] pt-[10px] text-[17px] max-w[30%]">
+                  <div>In the workforce, software engineering is one of the most demanding information careers. </div>
+                  <div className="pt-[10px]">With tech evoloving and everchanging, it is more important that ever to prioritize 
+                    your health as a software engineer. </div>
+                  <div className="pt-[10px]">This section will cover many topics including: </div>
+                  <li className="pt-[10px]">The Challenges</li>
+                  <li>Why It Matters</li>
+                  <li>Healthy Practices</li>
+                  <li>The Takeaway</li>
+                  <div className="pt-[10px]">Long hours, tight deadlines, and constant notifications can take a toll on both mind and body. Burnout, impostor feelings, eye strain, and poor posture are common issues. By taking care of your mental and physical health, you can stay productive, think clearly, and enjoy a sustainable career.</div>
+                  <div className="pt-[10px]">Focusing on wellness helps you build resilience, manage stress more effectively, and maintain a balance between work and life. With the right habits, you can approach your software engineering journey with energy, confidence, and a clearer sense of direction</div>
                 </div>
               </div>
               <Link className="flex justify-center bg-[#F0E4D3] hover:bg-gray-100 font-bold text-[30px] p-[50px] rounded-[15px] hover:rounded-[15px]" href="/topics/hub/health">Learn More</Link>
